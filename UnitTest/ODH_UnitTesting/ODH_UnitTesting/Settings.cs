536fcb05ba6879cc4d992d65d4ba3fbcc489982e
Josh Kulp <joshrkulp@gmail.com>
2020-10-26 13:50:23 -0400

    Update to allow for pipline

---
+++ UnitTest/ODH_UnitTesting/ODH_UnitTesting/Settings.cs
@@ -0,0 +1,8 @@
+﻿namespace ODH_UnitTesting
+{
+    internal class Settings
+    {
+        public string JmeterLogFileName { get; set; }
+        public string StatisticsFileName { get; set; }
+    }
+}
--- UnitTest/ODH_UnitTesting/ODH_UnitTesting/UnitTest1.cs
+++ UnitTest/ODH_UnitTesting/ODH_UnitTesting/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -14,25 +15,84 @@ namespace ODH_UnitTesting
     {
         private List<Result> _resultSet;
 
+
         [TestInitialize]
         public void Setup()
         {
-            var text = File.ReadAllText("./statistics.json");
+            var settings = File.ReadAllText(@".\appsettings.json");
+            var settingsObject = JsonConvert.DeserializeObject<Settings>(settings);
 
-            text = "[" + Regex.Replace(text.Remove(text.Length - 3).Remove(0, 2), "\".*: {", "{") + "]";
+            if (settingsObject.StatisticsFileName.Contains("__ResultsFolder__"))
+            {
+                settingsObject.StatisticsFileName = settingsObject.StatisticsFileName.Replace("__ResultsFolder__", ".");
+            }
+
+            var text = File.ReadAllText(settingsObject.StatisticsFileName);
+
+            text = "[" + Regex.Replace(text.Remove(text.Length - 1).Remove(0, 1), "\".*: {", "{") + "]";
 
             _resultSet = (JsonConvert.DeserializeObject<List<Result>>(text));
+
         }
 
         [TestMethod]
         [TestCategory("E Forms")]
         public void EFormSubmit()
         {
-            var result = _resultSet.FirstOrDefault(r => r.Transaction == "eForm Submit");
+            checkResults( "eForm Submit" );
+        }
 
-            Assert.IsTrue(result != null, $"No Results for eForm Submit");
-            Assert.IsTrue(result.ErrorPct <= 10, $"Error percentage for eForm Submit was {result.ErrorPct.ToString("N2")}%");
-            Assert.IsTrue(result.MeanResTime <= 200, $"Mean time for eForm Submit was {result.MeanResTime.ToString("N2")}ms");
+        [TestMethod]
+        [TestCategory("E Forms")]
+        public void GetStateCodes181()
+        {
+            checkResults("/Barcode/GetStateCodes-181");
+        }
+
+        [TestMethod]
+        [TestCategory("E Forms")]
+        public void GetStateCodes182()
+        {
+            checkResults("/Barcode/GetStateCodes-182");
+        }
+
+        [TestMethod]
+        [TestCategory("E Forms")]
+        public void Auth180()
+        {
+            checkResults("/Barcode/Auth-180");
+        }
+
+        [TestMethod]
+        [TestCategory("E Forms")]
+        public void Auth1801()
+        {
+            checkResults("/Barcode/Auth-180-1");
+        }
+
+        [TestMethod]
+        [TestCategory("E Forms")]
+        public void Auth1800()
+        {
+            checkResults("/Barcode/Auth-180-0");
+        }
+
+        [TestMethod]
+        [TestCategory("E Forms")]
+        public void ohid70()
+        {
+            checkResults("/wps/portal/gov/ohid-7-0");
+        }
+
+
+
+        public void checkResults(string title, int errorPct = 10, int meanResTime = 1000)
+        {
+            var result = _resultSet.FirstOrDefault(r => r.Transaction == title);
+
+            Assert.IsTrue(result != null, $"No Results for {title}");
+            Assert.IsTrue(result.ErrorPct <= errorPct, $"Error percentage for {title} was {result.ErrorPct.ToString("N2")}% and expected {errorPct}");
+            Assert.IsTrue(result.MeanResTime <= meanResTime, $"Mean time for {title} was {result.MeanResTime.ToString("N2")}ms and expected {meanResTime.ToString("N2")}ms");
         }
     }
 }
